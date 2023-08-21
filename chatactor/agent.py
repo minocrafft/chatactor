74efe745ef7da6c052d3e1a9314021e14e01a2fa
heonjin <87924725+kwon-evan@users.noreply.github.com>
2023-08-21 15:52:11 +0900

    refactor: short code

--- chatactor/agent.py
+++ chatactor/agent.py
@@ -5,17 +5,8 @@ from langchain.callbacks import StreamlitCallbackHandler
 from langchain.utilities import DuckDuckGoSearchAPIWrapper
 
 def get_agent(openai_api_key:str)-> AgentExecutor:
-    # Tools setup
-    search = DuckDuckGoSearchAPIWrapper(region="kr-kr")
     llm = OpenAI(temperature=0.5, openai_api_key=openai_api_key, streaming=True)
-    tools = [
-        Tool(
-            name="Search",
-            func=search.run,
-            description="useful for when you need to answer questions about current events. You should ask targeted questions",
-        ),
-    ] + load_tools(["wikipedia"], llm=llm)
-
+    tools = load_tools(["ddg-search", "wikipedia"], llm=llm)
     agent = initialize_agent(
         tools, 
         llm, 
