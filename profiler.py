067850e8a3bd44c6347986df8ed03379890644c2
heonjin <87924725+kwon-evan@users.noreply.github.com>
2023-08-26 17:08:33 +0900

    feat:modify profiler.py

--- profiler.py
+++ profiler.py
@@ -1,141 +1,61 @@
-import os
-import re
-from typing import List, Union
-
+from langchain.agents import ZeroShotAgent, Tool, AgentExecutor
 from langchain import LLMChain
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts import BaseChatPromptTemplate
-from langchain.agents import Tool, AgentExecutor, LLMSingleActionAgent, AgentOutputParser
 from langchain.utilities import DuckDuckGoSearchAPIWrapper, WikipediaAPIWrapper
-from langchain.tools import DuckDuckGoSearchRun, WikipediaQueryRun
-from langchain.schema import AgentAction, AgentFinish, HumanMessage
 
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", None)
+search = DuckDuckGoSearchAPIWrapper()
+wiki = WikipediaAPIWrapper()
+tools = [
+    Tool(
+        name="Search",
+        func=search.run,
+        description="Useful for when you want to search for something on the internet.",
+    ),
+    Tool(
+        name="Wikipedia",
+        func=wiki.run,
+        description="Useful for when you want to search for something on Wikipedia.",
+    ),
+]
 
-# Set up a prompt template
-class CustomPromptTemplate(BaseChatPromptTemplate):
-    # The template to use
-    template: str
-    # The list of tools available
-    tools: List[Tool]
-    
-    def format_messages(self, **kwargs) -> str:
-        # Get the intermediate steps (AgentAction, Observation tuples)
-        # Format them in a particular way
-        intermediate_steps = kwargs.pop("intermediate_steps")
-        thoughts = ""
-        for action, observation in intermediate_steps:
-            thoughts += action.log
-            thoughts += f"\nObservation: {observation}\nThought: "
-        # Set the agent_scratchpad variable to that value
-        kwargs["agent_scratchpad"] = thoughts
-        # Create a tools variable from the list of tools provided
-        kwargs["tools"] = "\n".join([f"{tool.name}: {tool.description}" for tool in self.tools])
-        # Create a list of tool names for the tools provided
-        kwargs["tool_names"] = ", ".join([tool.name for tool in self.tools])
-        formatted = self.template.format(**kwargs)
-        return [HumanMessage(content=formatted)]
+prefix = """
+You are a Entertainment Reporter. 
+You have been tasked to profile a given person.
+Do not use your own knowledge, use the internet to find the information.
 
-class CustomOutputParser(AgentOutputParser):
-    
-    def parse(self, llm_output: str) -> Union[AgentAction, AgentFinish]:
-        # Check if agent should finish
-        if "Final Answer:" in llm_output:
-            return AgentFinish(
-                # Return values is generally always a dictionary with a single `output` key
-                # It is not recommended to try anything else at the moment :)
-                return_values={"output": llm_output.split("Final Answer:")[-1].strip()},
-                log=llm_output,
-            )
-        # Parse out the action and action input
-        regex = r"Action\s*\d*\s*:(.*?)\nAction\s*\d*\s*Input\s*\d*\s*:[\s]*(.*)"
-        match = re.search(regex, llm_output, re.DOTALL)
-        if not match:
-            raise ValueError(f"Could not parse LLM output: `{llm_output}`")
-        action = match.group(1).strip()
-        action_input = match.group(2)
-        # Return the action and action input
-        return AgentAction(tool=action, tool_input=action_input.strip(" ").strip('"'), log=llm_output)
-
-def profiler_tools()->List[Tool]:
-    ddg_wrapper = DuckDuckGoSearchAPIWrapper(region="kr-kr", time='d')
-    search = DuckDuckGoSearchRun(api_wrapper=ddg_wrapper)
-    wiki_wrapper = WikipediaAPIWrapper(lang='ko')
-    wiki = WikipediaQueryRun(api_wrapper=wiki_wrapper)
-    tools = [
-        Tool(
-            name="Search",
-            func=search.run,
-            description="useful for when you need to answer questions about current events",
-        ),
-        Tool(
-            name="Wikipedia",
-            func=wiki.run,
-            description="useful for when you need to answer questions about historical events",
-        ),
-    ]
-    return tools
-
-def profiler_prompt(tools:List[Tool])->CustomPromptTemplate:
-    # Set up the base template
-    template = """Complete the objective as best you can. You have access to the following tools:
-
-    {tools}
-
-    Use the following format:
-
-    Person: the input person you must profile
-    Thought: you should always think about what to do
-    Action: the action to take, should be one of [{tool_names}]
-    Action Input: the input to the action
-    Observation: the result of the action
-    ... (this Thought/Action/Action Input/Observation can repeat N times)
-    Thought: I now can complete the profile
-    Final Answer: the final answer to the original input person. This must be a dictionary with the following keys: name, character, birth, death, history
-
-    These were previous tasks you completed:
+You have access to the following tools:"""
+suffix = """Begin! Remember to use the tools to find the information.
+The Final Answer is json formatted. as follows:
+You have to find the following information about the person:
+name: the person's name
+image: the url of the person's image
+occupation: the person's occupation
+speaking style: the person's speaking style
+birth: the birth of the person formatted in YYYY-MM-DD
+death: the death of the person formatted in YYYY-MM-DD
+events: List of the person's notable events while alive, the items of the list are formatted as follows:
+  event_name: the name of the event
+    event date: the date of the event formatted in YYYY-MM-DD
+    event description: A line of the description of the event
+  event_name: the name of the event ...
 
 
+Person: {input}
+{agent_scratchpad}"""
 
-    Begin!
+prompt = ZeroShotAgent.create_prompt(
+    tools, prefix=prefix, suffix=suffix, input_variables=["input", "agent_scratchpad"]
+)
 
-    Person: {input}
-    {agent_scratchpad}"""
+print(prompt.template)
 
+llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k")
+llm_chain = LLMChain(llm=llm, prompt=prompt)
 
-    prompt = CustomPromptTemplate(
-        template=template,
-        tools=tools,
-        # This omits the `agent_scratchpad`, `tools`, and `tool_names` variables because those are generated dynamically
-        # This includes the `intermediate_steps` variable because that is needed
-        input_variables=["input", "intermediate_steps"]
-    )
+tool_names = [tool.name for tool in tools]
+agent = ZeroShotAgent(llm_chain=llm_chain, allowed_tools=tool_names)
+agent_executor = AgentExecutor.from_agent_and_tools(
+    agent=agent, tools=tools, verbose=True
+)
 
-    return prompt
-
-
-def main():
-    output_parser = CustomOutputParser()
-    tools = profiler_tools()
-    prompt = profiler_prompt(tools)
-    tool_names = [tool.name for tool in tools]
-
-
-    llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, temperature=0)
-# LLM chain consisting of the LLM and a prompt
-    llm_chain = LLMChain(llm=llm, prompt=prompt)
-    tool_names = [tool.name for tool in tools]
-    agent = LLMSingleActionAgent(
-        llm_chain=llm_chain, 
-        output_parser=output_parser,
-        stop=["\nObservation:"], 
-        allowed_tools=tool_names
-    )
-    agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True)
-    agent_executor.run("이순신")
-
-if __name__ == "__main__":
-    main()
-
-
-
+agent_executor.run("뉴진스 하니")
