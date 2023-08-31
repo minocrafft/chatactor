8b3a70c0fe2caeb024dac702e95175e732dd4c1e
heonjin <87924725+kwon-evan@users.noreply.github.com>
2023-09-01 02:07:55 +0900

    feat: add stream chat

--- pages/chat.py
+++ pages/chat.py
@@ -1,15 +1,12 @@
+from pathlib import Path
 import streamlit as st
-from streamlit_chat import message
 from langchain.callbacks import StreamlitCallbackHandler
-from PIL import Image
 
-from chatactor import get_agent
+from chatactor import get_chatactor
 from functional.page import (
     set_page_config,
     description,
 )
-
-from chatactor.model import Actor
 from functional.component import settings
 
 
@@ -23,37 +20,50 @@ def draw_prechat():
             st.image(st.session_state.actor.image, width=300)
 
         with col2:
+            st.title(st.session_state.actor.name)
             st.markdown("\n\n * ".join(st.session_state.actor.content))
 
 
 def draw_chat():
-    st.title(st.session_state.actor.name)
+    if current_actor != st.session_state.actor:
+        st.session_state.messages.clear()
 
     # Display chat messages from history on app rerun
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-    agent = get_agent(st.session_state.openai_api_key)
+    agent = get_chatactor(
+        name=st.session_state.actor.name,
+        profiles_path=Path("profiles"),
+        openai_api_key=st.session_state.openai_api_key,
+    )
 
     prompt = st.chat_input("Message", key="message")
     if prompt:
         st.chat_message("user").write(prompt)
         st.session_state.messages.append({"role": "user", "content": prompt})
 
-    with st.chat_message("assistant"):
-        st_callback = StreamlitCallbackHandler(
-            st.container(),
-            max_thought_containers=int(st.session_state.max_thought_containers),
-            expand_new_thoughts=st.session_state.expand_new_thoughts,
-            collapse_completed_thoughts=st.session_state.collapse_completed_thoughts,
-        )
-        response = agent.run(prompt, callbacks=[st_callback])
-        st.write(response)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+        with st.chat_message("assistant"):
+            st_callback = StreamlitCallbackHandler(
+                st.container(),
+                max_thought_containers=int(st.session_state.max_thought_containers),
+                expand_new_thoughts=st.session_state.expand_new_thoughts,
+                collapse_completed_thoughts=st.session_state.collapse_completed_thoughts,
+            )
+            response = agent(prompt, callbacks=[st_callback])
+            output = response["output"]
+            st.write(output)
+            st.session_state.messages.append({"role": "assistant", "content": output})
+    else:
+        st.caption("Please enter a message to begin chatting.")
 
 
-if st.session_state.openai_api_key is None or not st.session_state.openai_api_key:
+if (
+    "openai_api_key" not in st.session_state
+    or not st.session_state.openai_api_key
+    or st.session_state.openai_api_key is None
+):
     description()
 
     st.caption("Please return to the home page and enter your :red[OpenAI API key].")
@@ -64,5 +74,6 @@ else:
     if not st.session_state.actor:
         st.subheader("캐릭터 화면으로 돌아가 캐릭터를 선택해주세요. :hugging_face:")
     else:
+        current_actor = st.session_state.actor
         draw_prechat()
         draw_chat()
