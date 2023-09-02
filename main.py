ddeabd36857b991bd74a5d6846272906dd8b49b3
minocrafft <minocrafft@gmail.com>
2023-09-02 21:11:45 +0900

    Change home

--- main.py
+++ main.py
@@ -13,25 +13,35 @@ set_page_config()
 
 with st.container():
     description()
+    apikey = st.secrets["OPENAI_API_KEY"]
+
+    run = st.button("시작하기", type="primary", use_container_width=True)
+
+    if run:
+        st.session_state.openai_api_key = apikey
+        st.success("API key set! \n\nYou can start chatting now!")
+
+        spinner("Loading...")
+        switch_page("actors")
 
     # check api key
-    apikey = os.getenv("OPENAI_API_KEY")
-    apikey = st.text_input(
-        label="🔑 Credentials - :red[OpenAI API Key]를 입력해주세요.",
-        type="password",
-        value=apikey,
-        placeholder="Input your OpenAI API Key!",
-    )
+    # apikey = os.getenv("OPENAI_API_KEY")
+    # apikey = st.text_input(
+    #     label="🔑 Credentials - :red[OpenAI API Key]를 입력해주세요.",
+    #     type="password",
+    #     value=apikey,
+    #     placeholder="Input your OpenAI API Key!",
+    # )
 
-    submit = st.button("Submit", type="primary")
+    # submit = st.button("Submit", type="primary")
 
-    if submit:
-        if not validate_openai_api_key(apikey):
-            st.session_state.openai_api_key = None
-            st.warning("Invalid OpenAI API Key...  \n\nPlease try again.")
-        else:
-            st.session_state.openai_api_key = apikey
-            st.success("API key set! \n\nYou can start chatting now!")
-
-            spinner("Loading...")
-            switch_page("actors")
+    # if submit:
+    #     if not validate_openai_api_key(apikey):
+    #         st.session_state.openai_api_key = None
+    #         st.warning("Invalid OpenAI API Key...  \n\nPlease try again.")
+    #     else:
+    #         st.session_state.openai_api_key = apikey
+    #         st.success("API key set! \n\nYou can start chatting now!")
+    #
+    #         spinner("Loading...")
+    #         switch_page("actors")
